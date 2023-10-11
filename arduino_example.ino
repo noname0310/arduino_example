a849c0ce957b72850ba98de37798fdbd198dd49e
noname <48761044+noname0310@users.noreply.github.com>
2023-10-11 11:37:19 +0900

    servo includes

--- arduino_example.ino
+++ arduino_example.ino
@@ -1,71 +1,19 @@
-// Arduino pin assignment
-#define PIN_LED 9
-#define PIN_TRIG 12 // sonar sensor TRIGGER
-#define PIN_ECHO 13 // sonar sensor ECHO
+#include <Servo.h>
+#define SERVO_PIN 10
 
-// configurable parameters
-#define SND_VEL 346.0 // sound velocity at 24 celsius degree (unit: m/sec)
-#define INTERVAL 25 // sampling interval (unit: msec)
-#define PULSE_DURATION 10 // ultra-sound Pulse Duration (unit: usec)
-#define DIST_MIN 100.0 // minimum distance to be measured (unit: mm)
-#define DIST_MAX 300.0 // maximum distance to be measured (unit: mm)
-#define TARGET_DISTANCE 200.0 // target distance (unit: mm)
-
-#define TIMEOUT ((INTERVAL / 2) * 1000.0) // maximum echo waiting time (unit: usec)
-#define DURATION2DISTANCE (0.001 * 0.5 * SND_VEL) // coefficent to convert duration to distance
-
-#define EMA_ALPHA 0.5 // EMA weight of new sample (range: 0 to 1). Setting EMA to 1 effectively disables EMA filter.
-
-unsigned long last_sampling_time; // unit: msec
-float prev_distance = DIST_MAX; // distance last-measured
-float ema_distance; // exponential moving average distance
+Servo servo;
 
 void setup() {
-    pinMode(PIN_LED, OUTPUT);
-    pinMode(PIN_TRIG, OUTPUT); // sonar TRIGGER
-    pinMode(PIN_ECHO, INPUT); // sonar ECHO
-    digitalWrite(PIN_TRIG, LOW); // turn-off Sonar
-
-    Serial.begin(57600);
+    servo.attach(SERVO_PIN);
+    servo.write(90);
+    delay(1000);
 }
 
 void loop() {
-    if (millis() < (last_sampling_time + INTERVAL)) return;
-
-    float raw_distance = measure_from_uss(PIN_TRIG, PIN_ECHO);
-    if (raw_distance == 0.0 || DIST_MAX < raw_distance || raw_distance < DIST_MIN) raw_distance = prev_distance;
-    ema_distance = EMA_ALPHA * raw_distance + (1.0 - EMA_ALPHA) * prev_distance;
-    prev_distance = ema_distance;
-
-    {
-        float distance_from_target = ema_distance;
-        if (distance_from_target < DIST_MIN) distance_from_target = DIST_MIN;
-        else if (distance_from_target > DIST_MAX) distance_from_target = DIST_MAX;
-        distance_from_target -= TARGET_DISTANCE;
-        distance_from_target = abs(distance_from_target);
-
-        int inv_brightness = distance_from_target * 255 / ((DIST_MAX - DIST_MIN) / 2);
-        analogWrite(PIN_LED, inv_brightness);
-    }
-
-    Serial.print("Min:");
-    Serial.print(DIST_MIN);
-    Serial.print(",raw_distance:");
-    Serial.print(raw_distance);
-    Serial.print(",ema_distance:");
-    Serial.print(ema_distance);
-    Serial.print(",Max:");
-    Serial.print(DIST_MAX);
-    Serial.println("");
-
-    last_sampling_time = millis();
-}
-
-// get a distance reading from USS. return value is in millimeter.
-float measure_from_uss(int trigger, int echo) {
-    digitalWrite(trigger, HIGH);
-    delayMicroseconds(PULSE_DURATION);
-    digitalWrite(trigger, LOW);
-
-    return pulseIn(echo, HIGH, TIMEOUT) * DURATION2DISTANCE; // unit: mm
+    servo.write(0);
+    delay(1000);
+    servo.write(90);
+    delay(1000);
+    servo.write(180);
+    delay(1000);
 }
